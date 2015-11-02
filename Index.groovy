5e4819d83d479c008b8b0da8afdfd1faa1a3cfb6
happykiller <fabrice.rosito@gmail.com>
2015-11-02 18:35:36 +0100

    init

---
+++ Index.groovy
@@ -0,0 +1,23 @@
+import groovy.json.JsonBuilder
+import org.bonitasoft.console.common.server.page.*
+import org.bonitasoft.engine.api.TenantAPIAccessor
+
+import javax.servlet.http.HttpServletRequest
+
+public class Get implements RestApiController {
+
+    @Override
+    RestApiResponse doHandle(HttpServletRequest request, PageResourceProvider pageResourceProvider, PageContext pageContext, RestApiResponseBuilder apiResponseBuilder, RestApiUtil restApiUtil) {
+
+        def session = pageContext.getApiSession();
+        def processAPI = TenantAPIAccessor.getProcessAPI(session);
+
+        Map<String, String> response = [:]
+        response.put "response", "hello from get resource"
+        response.putAll request.parameterMap
+        apiResponseBuilder.with {
+            withResponse new JsonBuilder(response).toPrettyString()
+            build()
+        }
+    }
+}
